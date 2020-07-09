45f2900592825d0ed34520198d88d0c224c6449b
Hamza Ahmed <hamza.ahmed0402@gmail.com>
2020-07-09 18:25:27 +0800

    set background of items in sidebar dynamically

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './index.css';
 import balls from './../../assets/ball_s.png'
 import home_icon from './../../assets/home.png'
@@ -7,13 +7,19 @@ import pointstable_icon from './../../assets/pointstable.png'
 import scoring_icon from './../../assets/scoring.png'
 import mom from './../../assets/mom.png'
 
-import {useDispatch} from 'react-redux'
+import {useDispatch,useSelector} from 'react-redux'
 
 import { getHomeComponent,getTeamsComponent,getPointsTableComponent,getScoreComponent,getMOMComponent } from '../../actions/sidebar';
 
 const Sidebar = () => {
 
   const dispatch = useDispatch()
+
+  const statusHome = useSelector(state => state.sidebar.HomeStatus);
+  const statusTeams = useSelector(state => state.sidebar.TeamSatus);
+  const statusPointsTable = useSelector(state => state.sidebar.PointsTableSatus);
+  const statusScoring = useSelector(state => state.sidebar.ScoreSatus);
+  const statusMom = useSelector(state => state.sidebar.MomSatus);
 
   const handleClickHome = (event) => {
     dispatch(getHomeComponent())
@@ -37,24 +43,29 @@ const Sidebar = () => {
       <img src={balls} width={130} height={130} />
 
       <div className="items">
-        <a onClick ={handleClickHome.bind(this)}href="#" className="first">
+      <a style={{backgroundColor: statusHome ? '#e0e0e0' : '#ffffff'}}
+        onClick ={handleClickHome.bind(this)}href="#" className="first">
           <img src={home_icon} width={20} height={20} />
           <span className="home_text">Home</span>
         </a>
 
-        <a onClick ={handleClickTeams.bind(this)} href="#" >
+        <a style={{backgroundColor: statusTeams ? '#e0e0e0' : '#ffffff'}}
+        onClick ={handleClickTeams.bind(this)} href="#" >
           <img src={teams_icon} width={20} height={20} />
           <span className="home_text">Teams</span>
         </a>
-        <a onClick ={handleClickPointsTable.bind(this)} href="#" >
+        <a style={{backgroundColor: statusPointsTable ? '#e0e0e0' : '#ffffff'}}
+        onClick ={handleClickPointsTable.bind(this)} href="#" >
           <img src={pointstable_icon} width={20} height={20} />
           <span className="home_text">Points Table</span>
         </a>
-        <a onClick ={handleClickScore.bind(this)} href="#" >
+        <a style={{backgroundColor: statusScoring ? '#e0e0e0' : '#ffffff'}}
+        onClick ={handleClickScore.bind(this)} href="#" >
           <img src={scoring_icon} width={20} height={20} />
           <span className="home_text">Scoring</span>
         </a>
-        <a onClick ={handleClickMom.bind(this)} href="#" >
+        <a style={{backgroundColor: statusMom ? '#e0e0e0' : '#ffffff'}}
+        onClick ={handleClickMom.bind(this)} href="#" >
           <img src={mom} width={20} height={20} />
           <span className="home_text">Man of the match</span>
         </a>
